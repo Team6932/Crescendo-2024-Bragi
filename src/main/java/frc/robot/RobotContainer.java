cee976f4e1dc64b8afc8e5d69d2596aa88453cee
liangyuzhao6932 <158101117+liangyuzhao6932@users.noreply.github.com>
2024-02-26 17:09:50 -0500

    shooter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.AbsoluteDrive;
+import frc.robot.subsystems.ShootSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
 import java.io.File;
@@ -29,8 +30,8 @@ import java.io.File;
 public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
-  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
-                                                                         "swerve"));
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  private final ShootSubsystem shootSubsystem = new ShootSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   PS4Controller driveController = new PS4Controller(0);
@@ -88,8 +89,17 @@ public class RobotContainer {
    * {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4}
    * controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
    */
-  private void configureBindings()
-  {
+  private void configureBindings() {
+
+    Trigger halfSpeed = new Trigger(() -> driveController.getL1Button()); // if L1 on drive, half speed
+		Trigger halfTurn = new Trigger (() -> pieceController.getR1Button()); // if R1 on piece, half turn speed	
+		Trigger leftShoot = new Trigger(() -> pieceController.getCircleButton()); // if Circle on piece, turn on left shoot
+		Trigger rightShoot = new Trigger(() -> pieceController.getTriangleButton()); // if Circle on piece, turn on right shoot
+		Trigger resetHeading = new Trigger(() -> driveController.getOptionsButton()); // if Options on drive, reset heading
+		Trigger shoot = new Trigger(() -> pieceController.getL1Button()); // if L1 on piece, shoot
+
+    shoot.whileTrue(new Shoot(shootSubsystem, 0.5, 0.5));
+
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
 
     /* driveController.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
